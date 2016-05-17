b5815c1d61f18412aa25f8d88239df4e896ee784
arainrabee19 <bsef14m523@pucit.edu.pk>
2016-05-17 23:48:11 +0500

    Create Client2.py

---
+++ Client2.py
@@ -0,0 +1,25 @@
+import socket  # Import socket module
+from thread import *
+
+s = socket.socket()
+host = socket.gethostname()
+port = 5173  # Reserve a port for your service.
+
+s.connect((host, port))
+print s.recv(1024)
+
+def receive():
+    while True:
+        data=s.recv(1024)
+        print data
+
+start_new_thread(receive, ())
+while 1:
+    rep=raw_input()
+    s.send(rep)
+    if not rep:
+        break
+
+
+
+s.close
